b7c4e5a3f9ffca582ab477e1aaeb47255ad8f948
Tim Taylor <tim@tool-man.org>
2012-06-19 17:45:27 -0400

    Initial Commit

---
+++ kiwi-bdd-ios/KBDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KBDAppDelegate.h
+//  kiwi-bdd-ios
+//
+//  Created by Tim Taylor on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kiwi-bdd-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  kiwi-bdd-ios
+//
+//  Created by Tim Taylor on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBDAppDelegate class]));
+    }
+}
